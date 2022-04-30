5c37ac37f1d0178c801502e454cd1aef959c1397
Jens Johnston <johnstonjr27@gmail.com>
2022-04-30 10:57:47 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      'zinc': {
+        100: '#cffafe',
+        200: '#a5f3fc',
+        300: '#67e8f9',
+        400: '#22d3ee',
+        500: '#06b6d4',
+        600: '#0891b2',
+        700: '#0e7490',
+        800: '#155e75',
+        900: '#164e63',
+      },
     extend: {
       colors: {
-        'body': '#17171F',
         'selected-text': '#A3A3FF',
         'theme': '#3F3FFF',
         'nav': '#404053',
@@ -13,9 +25,6 @@ module.exports = {
         'input-border': '#565666',
         'input': '#2A2A35'
       },
-      fontFamily: {
-        'poppins': ["'Poppins'", 'sans-serif']
-      }
     },
   },
   variants: {
